44f5ae29d012820ffe3963d1dad62b7b3137ddec
laibaijiang <laibaijiang@lixiang.com>
2023-06-16 18:03:58 +0800

    变换操作符

--- app/src/main/java/com/lixiang/rxjavademo/MainActivity.java
+++ app/src/main/java/com/lixiang/rxjavademo/MainActivity.java
@@ -16,6 +16,9 @@ import com.lixiang.rxjavademo.createOperator.normal.Create;
 import com.lixiang.rxjavademo.createOperator.normal.FromArray;
 import com.lixiang.rxjavademo.createOperator.normal.FromIterable;
 import com.lixiang.rxjavademo.createOperator.normal.Just;
+import com.lixiang.rxjavademo.switchOperator.SwitchConcatMap;
+import com.lixiang.rxjavademo.switchOperator.SwitchFlatMap;
+import com.lixiang.rxjavademo.switchOperator.SwitchMap;
 
 public class MainActivity extends AppCompatActivity {
     Button createBtn;
@@ -27,12 +30,48 @@ public class MainActivity extends AppCompatActivity {
     Button intervalRangeBtn;
     Button rangeBtn;
     Button timerBtn;
+    Button mapBtn;
+    Button flatMapBtn;
+    Button concatMapBtn;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createOperator();
+
+        switchOperator();
+
+    }
+
+    private void switchOperator() {
+        mapBtn = findViewById(R.id.map);
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchMap.use();
+            }
+        });
+
+        flatMapBtn = findViewById(R.id.flatMap);
+        flatMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchFlatMap.use();
+            }
+        });
+
+        concatMapBtn = findViewById(R.id.concatMap);
+        concatMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchConcatMap.use();
+            }
+        });
+    }
+
+    private void createOperator() {
         normal();
 
         delay();
